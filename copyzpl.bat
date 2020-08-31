b8d465da067a47480ffb5586dcb228c5a3924fde
EthanVVLulus <ethan.vanvalen@penn.lulus.network>
2020-08-31 15:33:32 -0400

    This is my first commit!

---
+++ copyzpl.bat
@@ -0,0 +1,3 @@
+mkdir %USERPROFILE%\AppData\Local\Zebra
+mkdir %USERPROFILE%\AppData\Local\Zebra\BrowserPrint
+xcopy C:\Users\Default\AppData\Local\Zebra\BrowserPrint\*.* %USERPROFILE%\AppData\Local\Zebra\BrowserPrint /I /Y
